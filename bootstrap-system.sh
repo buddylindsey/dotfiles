970323c3fde561c25715cbd4ae57be0ab35678d4
Buddy Lindsey, Jr <buddylindsey@users.noreply.github.com>
2021-12-12 14:08:06 -0600

    add dev branch and app todo

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -16,8 +16,8 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-OPTIONS=cgv
-LONGOPTS=console,gui,verbose
+OPTIONS=cgdv
+LONGOPTS=console,gui,dev,verbose
 
 # -regarding ! and PIPESTATUS see above
 # -temporarily store output to be able to check for errors
@@ -32,7 +32,7 @@ fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
-c=n g=n v=n
+c=n g=n v=n d=n
 # now enjoy the options in order and nicely split until we see --
 while true; do
     case "$1" in
@@ -48,6 +48,10 @@ while true; do
             v=y
             shift
             ;;
+        -d|--dev)
+            d=y
+            shift
+            ;;
         --)
             shift
             break
@@ -59,17 +63,25 @@ while true; do
     esac
 done
 
-echo "console: $c, gui: $g, verbose: $v"
+echo "console: $c, gui: $g, dev: $d, verbose: $v"
 
 PREFLIGHT_CHECK_SUCCESS="true"
 
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
 # Check if a command exists. Then replace.
 app_installed() {
-    if ! command -v $1 &> /dev/null
+    if ! command_exists $1
     then
         PREFLIGHT_CHECK_SUCCESS="false"
         echo "$1 is not installed"
     fi
+}
+
+folder_exists() {
+    [ -d "$1" ]
 }
 
 is_console() {
@@ -84,6 +96,10 @@ is_verbose() {
     [[ $v = 'y' ]]
     return
 }
+is_dev() {
+    [[ $d = 'y' ]]
+    return
+}
 
 console_copy() {
     is_console && cp $1 $2 && echo "Copied $2"
@@ -91,7 +107,7 @@ console_copy() {
 
 # Preflight check
 
-CONSOLE_APPS=("git" "htop" "batcat" "exa" "youtube-dl" "pianobar" "curl" "wget")
+CONSOLE_APPS=("git" "htop" "batcat" "exa" "youtube-dl" "pianobar" "curl" "wget" "neofetch")
 GUI_APPS=("polybar" "rofi")
 
 function app_check() {
@@ -111,9 +127,46 @@ fi
 
 # Do install of console specific apps and configs
 
-console_copy ./files/gitconfig ~/.gitconfig
+is_console && echo "Copying over root level configs"
+is_console && console_copy ./files/gitconfig ~/.gitconfig
 
-# 1. Install oh-my-zsh
-# 2. Install starship
-# 3. Copy over zshrc plugin
-console_copy ./files/zshrc ~/.zshrc
+is_console && echo "Setting up command prompt"
+
+if [ ! -d ~/.oh-my-zsh ]; then
+    is_console && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+is_console && sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+
+# 3. Install nerd font
+is_console && console_copy ./files/zshrc ~/.zshrc
+
+is_console && console_copy ./files/tmux.conf ~/.tmux.conf
+# TODO: Clone TPM execute install of plugins
+
+# To install - console
+# 1. pianobar
+# 2. youtube-dl
+# 3. ytop
+
+
+# To install - dev
+# 1. asdf-vm
+# 2. rustup
+# 3. python
+# 4. nodejs
+# 5. postgresql client apps
+# 6. docker
+# 7. redis-cli
+
+# To install - gui
+# 1. element.io
+# 2. joplin app
+# 3. Alacritty
+# 4. polybar
+# 5. rofi
+# 6. teams
+# 7. Micorosft Edge
+# 8. Brave Browser
+# 9. Chromium
+#10. Telegram
+#11. QTile
